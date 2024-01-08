515ef523d18ccba89ee24c50fcd3cf0aa2ff7bc3
Ryu <65792780+Ryu-123-dev@users.noreply.github.com>
2024-01-08 16:31:05 -0600

    Updated json, n More
    Added SwerveSubsystem to have "pushOffsetsToControllers()" method.
    
    JSON:
    Updated basic things like offset to be correct, and double checked conversion math

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -92,6 +92,7 @@ public class SwerveSubsystem extends SubsystemBase
   public SwerveSubsystem(SwerveDriveConfiguration driveCfg, SwerveControllerConfiguration controllerCfg)
   {
     swerveDrive = new SwerveDrive(driveCfg, controllerCfg, maximumSpeed);
+    swerveDrive.pushOffsetsToControllers();
   }
 
   /**
